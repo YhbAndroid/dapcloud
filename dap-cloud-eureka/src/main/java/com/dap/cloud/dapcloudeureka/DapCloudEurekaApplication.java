1df43292fecab8b053f54b7636a0bbb6da241137
yuan <yhbandroid@163.com>
2020-04-29 10:20:29 +0800

    first commit

---
+++ dap-cloud-eureka/src/main/java/com/dap/cloud/dapcloudeureka/DapCloudEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.dap.cloud.dapcloudeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DapCloudEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DapCloudEurekaApplication.class, args);
+    }
+
+}
