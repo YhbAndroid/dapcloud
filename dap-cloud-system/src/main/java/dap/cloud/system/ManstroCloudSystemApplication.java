e1c03a3de7773eb9a95110036171211b0e5990d3
yuan <yhbandroid@163.com>
2020-04-29 13:10:49 +0800

    system perfect

--- dap-cloud-system/src/main/java/dap/cloud/system/ManstroCloudSystemApplication.java
+++ dap-cloud-system/src/main/java/dap/cloud/system/ManstroCloudSystemApplication.java
@@ -2,8 +2,14 @@ package dap.cloud.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableRedisHttpSession
+@EnableScheduling
 public class ManstroCloudSystemApplication {
 
     public static void main(String[] args) {
