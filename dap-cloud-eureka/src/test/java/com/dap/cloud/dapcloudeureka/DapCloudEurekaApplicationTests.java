697ba130aced842b5138088582323e7b16d06212
yuan <yhbandroid@163.com>
2020-04-29 11:01:08 +0800

    dap-cloud-eureka

---
+++ dap-cloud-eureka/src/test/java/com/dap/cloud/dapcloudeureka/DapCloudEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dap.cloud.dapcloudeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DapCloudEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
