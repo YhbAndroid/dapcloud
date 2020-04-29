f734862650a03317ffaa8691cb32d12d12dc06da
yuan <yhbandroid@163.com>
2020-04-29 13:09:11 +0800

    add module system

---
+++ dap-cloud-system/src/main/java/dap/cloud/system/ManstroCloudSystemApplication.java
@@ -0,0 +1,13 @@
+package dap.cloud.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManstroCloudSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManstroCloudSystemApplication.class, args);
+    }
+
+}
